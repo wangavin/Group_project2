ebc0188509a4c66b014acb7622df71c84fedbe9a
Gavin <gavinwan0929@gmail.com>
2023-03-26 18:54:16 -0400

    gro2

---
+++ ERD_Schema/Schema/Schema_Group3.sql
@@ -0,0 +1,60 @@
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(64)   NOT NULL,
+    "description" varchar(64)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar(64)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(64)   NOT NULL,
+    "currency" varchar(64)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(64)   NOT NULL,
+    "subcategory_id" varchar(64)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" varchar(64)   NOT NULL,
+    "category" varchar(64)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(64)   NOT NULL,
+    "last_name" varchar(64)   NOT NULL,
+    "email" varchar(128)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar(64)   NOT NULL,
+    "subcategory" varchar(64)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+SELECT * FROM campaign
+SELECT * FROM category
+SELECT * FROM contacts
+SELECT * FROM subcategory
---
+++ README.md
@@ -0,0 +1,34 @@
+# Module-13-Project2-ETL
+
+## Project Description
+The module 13 ETL mini project is focused on building an ETL popline using Python, Pandas, and either Python dictionary methods or regular expressions to extract and transform the data. After the data has been transformed, 4 CSV files will be created from the cleaned up data frames. These 4 CSV will be used to to create an ERD and the CSV file data will also be uploaded into a Postgres database.
+
+
+## About
+The files uses in this challenge will in in jupyter notebook file format and SQL format 
+
+## Files 
+Main file:<br />
+ETL_Mini_Project_Group_3.ipynb<br />
+<br />
+Individual files: <br />
+ETL_Mini_Project_ADeng.ipynb<br />
+ETL_Mini_Project_SZAND.ipynb<br />
+ETL_Mini_Project_ZCORBETT.ipynb<br />
+GW1_ETL_Mini_Project_Starter_Code.ipynb
+<br />
+Resources(Folder) <br />
+ERD_Group_3.png <br />
+Schema_Group_3.sql 
+
+## Contributing
+Zac <br />
+Gavin <br />
+Alan <br />
+Set<br />
+
+## Resources
+Stack Overflow, W3school <br />
+Tom Seeber<br />
+Jordan Merritt
+
